d633b7a52c3700a841af55bf61245e460d6790ef
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-11-07 17:39:06 +0300

    greenStep: testing constructor Node

--- src/test/java/com/stepan/avl/TestingAVL.java
+++ src/test/java/com/stepan/avl/TestingAVL.java
@@ -1,8 +1,32 @@
 package com.stepan.avl;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TestingAVL {
 
+    @Test
+    public void testConstructorNode() {
+        Node node = new Node(1, 1, null);
+
+        int h = node.h;
+        int balance = node.balance;
+        int key = node.key;
+        int value = node.value;
+        Node left = node.left;
+        Node right = node.right;
+        Node father = node.father;
+
+        assertEquals(h, 1);
+        assertEquals(balance, 0);
+        assertEquals(key, 1);
+        assertEquals(value, 1);
+        assertNull(left);
+        assertNull(right);
+        assertNull(father);
+    }
 
 }
