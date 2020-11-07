025099ad9b5818f2d3a85159ff8981a3899d57a9
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-11-07 17:48:00 +0300

    feat: add method height

--- src/main/java/com/stepan/avl/AVL.java
+++ src/main/java/com/stepan/avl/AVL.java
@@ -3,4 +3,10 @@ package com.stepan.avl;
 public class AVL {
     public Node root;
 
+    public int height(Node x, Node y) {
+        if (x == null && y == null) return 0;
+        else if (x == null) return y.h;
+        else if (y == null) return x.h;
+        else return Math.max(x.h, y.h);
+    }
 }
