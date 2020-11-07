b7dd8c01e92e1deb411f3f8d20a5f382cd5b6b59
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-11-07 17:37:17 +0300

    fix: change Integer to int

--- src/main/java/com/stepan/avl/Node.java
+++ src/main/java/com/stepan/avl/Node.java
@@ -3,11 +3,11 @@ package com.stepan.avl;
 public class Node {
     public int h;
     public int balance;
-    public Integer key;
-    public Integer value;
+    public int key;
+    public int value;
     public Node left, right, father;
 
-    public Node(Integer key, Node father) {
+    public Node(int key, int value, Node father) {
         this.key = key;
         this.value = value;
         this.left = this.right = null;
