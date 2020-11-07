3bfc9d4be21106173cf25aeb17e65a4102e9677a
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-11-07 18:04:22 +0300

    feat: add balance method

--- src/main/java/com/stepan/avl/AVL.java
+++ src/main/java/com/stepan/avl/AVL.java
@@ -9,4 +9,11 @@ public class AVL {
         else if (y == null) return x.h;
         else return Math.max(x.h, y.h);
     }
+
+    public int balance(Node x, Node y) {
+        if (x == null && y == null) return 0;
+        else if (x == null) return -y.h;
+        else if (y == null) return x.h;
+        else return x.h - y.h;
+    }
 }
